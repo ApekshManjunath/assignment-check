c9952224871f6734bcb61d74d3b49daa66e45f24
ApekshManjunath <46856095+ApekshManjunath@users.noreply.github.com>
2023-10-30 18:42:30 -0700

    Update Duplicate_example.py

--- Example-main/Example-main/Duplicate_example.py
+++ Example-main/Example-main/Duplicate_example.py
@@ -13,3 +13,6 @@ def calculate_volume(length, width, height):
     volume = length * width * height
     return volume
 
+def calculate_surface_area(length, width, height):
+    surface_area = 2 * (length * width + length * height + width * height)
+    return surface_area
