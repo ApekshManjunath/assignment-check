ecfc8158af42eef65c5a0951995cd4393ca1de6b
ApekshManjunath <46856095+ApekshManjunath@users.noreply.github.com>
2023-10-30 18:43:19 -0700

    Update Duplicate_example.py

--- Example-main/Example-main/Duplicate_example.py
+++ Example-main/Example-main/Duplicate_example.py
@@ -12,7 +12,3 @@ def calculate_perimeter(length, width):
 def calculate_volume(length, width, height):
     volume = length * width * height
     return volume
-
-def calculate_surface_area(length, width, height):
-    surface_area = 2 * (length * width + length * height + width * height)
-    return surface_area
